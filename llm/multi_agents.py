ed09f74776e13a7afc7bf1049fa402cb86b35029
justas145 <justasand1@gmail.com>
2024-07-02 09:26:43 +0200

    cleaner code

--- llm/multi_agents.py
+++ llm/multi_agents.py
@@ -3,6 +3,7 @@ from agent_tools import (
     initialize_client,
     initialize_collection,
     agent_tools_list,
+    agent_tool_dict
 )
 import os
 import sys
@@ -13,7 +14,12 @@ from io import StringIO
 import chromadb
 import dotenv
 from langchain import agents, hub
-from langchain.agents import AgentExecutor, create_structured_chat_agent, create_react_agent
+from langchain.agents import (
+    AgentExecutor,
+    create_structured_chat_agent,
+    create_react_agent,
+    create_openai_tools_agent,
+)
 from langchain_community.chat_models import ChatOllama
 from langchain_core.messages import AIMessage, HumanMessage
 
@@ -23,20 +29,29 @@ from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 sys.path.append("../bluesky")
 from bluesky.network.client import Client
-
+import json
+from prompts.agent_prompts import (
+    planner_prompt,
+    executor_prompt,
+    verifier_prompt,
+    all_prompt,
+)
 
 # %%
 dotenv.load_dotenv("../.env")
 
-temperature = 0.3
-model_name = "llama3-70b-8192"
+temperature = 0.4
+model_name = "Llama3-70b-8192"
+chat = ChatGroq(temperature=temperature, model_name=model_name)
 
+#chat = ChatOpenAI(model='gpt-4o', temperature=0.1)
 
 # %%
 # Initialization vector db
 
 base_path = os.path.dirname(__file__)
 vectordb_path = os.path.join(base_path, "skills-library", "vectordb")
+prompts_path = os.path.join(base_path, "prompts", "prompts.py")
 
 chroma_client = chromadb.PersistentClient(path=vectordb_path)
 
@@ -53,7 +68,7 @@ client.connect("127.0.0.1", 11000, 11001)
 client.update()
 # Initialize the client in agent_tools
 initialize_client(client)
-
+print("Connected to BlueSky")
 
 # %%
 # Vector DB
@@ -70,30 +85,37 @@ collection = chroma_client.get_or_create_collection(
 initialize_collection(collection)
 
 
-# %%
+def get_intermediate_steps(data):
+    steps_summary = []
+    if "intermediate_steps" in data:
+        for step in data["intermediate_steps"]:
+            action = step[0].tool
+            command = json.dumps(step[0].tool_input)
+            # Extracting the response message from log
+            invoke = step[0].log
+            response = step[1]
 
-prompt = hub.pull("hwchase17/openai-tools-agent")
+            summary = f"Action: {action}, Command: {command}, Invoke: {invoke}, Response: {response}"
+            steps_summary.append(summary)
 
-with open("prompts/system.txt", "r") as f:
-    prompt.messages[0].prompt.template = f.read()
-
-with open("prompts/conflict.txt", "r") as f:
-    prompt.messages[0].prompt.template += f.read()
+    return "\n\n".join(steps_summary)
 
 
-chat = ChatGroq(temperature=0.2, model_name=model_name)
 # chat = ChatOpenAI(temperature=0.2, model_name='gpt-4o')
 # chat = ChatOllama(
 #     base_url="https://ollama.junzis.com", model="mixtral:8x22b", temperature=0.2
 # )
-agent_tools_list_planner = agent_tools_list[0:2]
-agent_tool_list_controller = [agent_tools_list[i] for i in (0, 2)]
-agent_tool_list_retriever = [agent_tools_list[-1]]
-agent_tool_list_verifier = [agent_tools_list[i] for i in (0, 1, 4)]
-# Get the prompt to use - you can modify this!
-prompt = hub.pull("hwchase17/structured-chat-agent")
+agent_tools_list_planner = [agent_tool_dict[tool] for tool in ["GETALLAIRCRAFTINFO", "GETCONFLICTINFO"]]
+agent_tools_list_controller = [agent_tool_dict[tool] for tool in ["SENDCOMMAND"]]
+agent_tools_list_verifier = [
+    agent_tool_dict[tool] for tool in ["GETALLAIRCRAFTINFO", "GETCONFLICTINFO", "CONTINUEMONITORING"]
+]
 
-agent_planner = create_structured_chat_agent(chat, agent_tools_list_planner, prompt)
+
+system_prompt = hub.pull("hwchase17/openai-tools-agent")
+
+
+agent_planner = create_openai_tools_agent(chat, agent_tools_list_planner, system_prompt)
 agent_executor_planner = AgentExecutor(
     agent=agent_planner,
     tools=agent_tools_list_planner,
@@ -102,110 +124,90 @@ agent_executor_planner = AgentExecutor(
     return_intermediate_steps=True,
 )
 
-agent_controller = create_structured_chat_agent(
-    chat, agent_tool_list_controller, prompt
+agent_controller = create_openai_tools_agent(
+    chat, agent_tools_list_controller, system_prompt
 )
 agent_executor_controller = AgentExecutor(
     agent=agent_controller,
-    tools=agent_tool_list_controller,
+    tools=agent_tools_list_controller,
     verbose=True,
     handle_parsing_errors=True,
     return_intermediate_steps=True,
 )
 
-agent_retriever = create_structured_chat_agent(chat, agent_tool_list_retriever, prompt)
-agent_executor_retriever = AgentExecutor(
-    agent=agent_retriever,
-    tools=agent_tool_list_retriever,
-    verbose=True,
-    handle_parsing_errors=True,
-    return_intermediate_steps=True,
-)
 
-agent_verifier = create_structured_chat_agent(chat, agent_tool_list_verifier, prompt)
+agent_verifier = create_openai_tools_agent(
+    chat, agent_tools_list_verifier, system_prompt
+)
 agent_executor_verifier = AgentExecutor(
     agent=agent_verifier,
-    tools=agent_tool_list_verifier,
+    tools=agent_tools_list_verifier,
     verbose=True,
     handle_parsing_errors=True,
     return_intermediate_steps=True,
 )
-
-# agent = agents.create_openai_tools_agent(chat, agent_tools_list, prompt)
-
-# agent_executor = agents.AgentExecutor(
-#     agent=agent, tools=agent_tools_list, verbose=True)
-
-
 # %%
 # planner
 
+with open("prompts/ICAO_seperation_guidelines.txt", "r") as f:
+    icao_seperation_guidelines = f.read()
 
+
+planner_prompt = planner_prompt.format(
+    icao_seperation_guidelines=icao_seperation_guidelines
+)
+scenario = 'case7'
+client.send_event(b'STACK', f'IC simple/conflicts/2ac/{scenario}.scn')
+time.sleep(1.5)
+client.update()
+
+
+planner_output_lst = []
+planner_intermediate_steps_lst = []
+controller_output_lst = []
+controller_intermediate_steps_lst = []
+verifier_output_lst = []
+verifier_intermediate_steps_lst = []
+
+planner_output = agent_executor_planner.invoke({"input": planner_prompt})
+input = planner_output["input"]
+plan = planner_output["output"]
+planner_output_lst.append(plan)
+planner_intermediate_steps_lst.append(
+    get_intermediate_steps(planner_output)
+)
+if "no conflicts" in plan:
+    print(plan)
+    exit()
 while True:
-
-    start_input = """
-    Analyze the conflicts between aircraft, identify the type of conflicts and the seperation requirements from ICAO. As a final answer provide an actionable plan to resolve the conflict. For example:
-    1. <Aircraft ID> <Action> <Reason and ICAO requirement>.
-    2. <Aircraft ID> <Action> <Reason and ICAO requirement>.
-    3. <Aircraft ID> <Action> <Reason and ICAO requirement>.
-    4. ...
-    ...
-    INSTRUCTIONS:
-    Always use absolute values and not relative values. For example use change heading to ABSOLUTE VALUE instead of change heading by RELATIVE VALUE.
-    
-    Always include current values into the action plan. For example if the plan is to maintain current heading, include the current heading value in the plan. 
-
-    If there are no conflicts, respond with final answer as: NO CONFLICTS
-    """
-
-    planner_output = agent_executor_planner.invoke({"input": start_input})
-    plan = planner_output["output"]
-
-    if "no conflicts" in plan:
-        print(plan)
-        exit()
-
-    # commands.txt path is base path + prompts + commands.txt
-    commands_path = os.path.join(base_path, "prompts", "commands.txt")
-
-    # read commands and use base_commands variable
-    with open(commands_path, "r") as f:
-        base_commands = f.read()
-
-    retriever_prompt = f"""Find  necesarry commands that are needed to execute the plan.
-
-    <Plan>: 
-    {plan}
-    <Plan>
-
-    <Commands>
-    {base_commands}
-    <Commands>
-
-    Present your answer in this way:
-    1. <Command> 
-    <Description of command including the meanings of the arguments>
-    <Command Syntax>
-    2. ...
-    3. ...
-    
-    """
-
-    retriever_output = agent_executor_retriever.invoke({"input": retriever_prompt})
-    bluesky_commands = retriever_output["output"]
-
-    controller_prompt = f"""Execute: {plan}
-    Here are some commands that will help you execute the plan:
-    {bluesky_commands}
-    """
+    controller_prompt = executor_prompt.format(plan=plan)
     controller_output = agent_executor_controller.invoke({"input": controller_prompt})
-    verifier_prompt = f""" Verify that the conflict has been resolved. This was the conflict resolution plan: 
-    {plan}
-    
-    Monitor the airspace to see if the conflict has been resolved or is in the process of being resolved. Return your answer as either: Conflicts resolved or Conflicts not resolved.
-    """
+    controller_output_lst.append(controller_output["output"])
+    controller_intermediate_steps_lst.append(get_intermediate_steps(controller_output))
+
+    verifier_prompt = verifier_prompt.format(
+        icao_seperation_guidelines=icao_seperation_guidelines, plan=plan
+    )
     verifier_output = agent_executor_verifier.invoke({"input": verifier_prompt})
-    if 'not' or 'no' in verifier_output["output"].lower():
-        continue
-    else:
+    verifier_output_lst.append(verifier_output["output"])
+    verifier_intermediate_steps_lst.append(get_intermediate_steps(verifier_output))
+    replan = verifier_output["output"]
+    if "no conflicts" in replan.lower():
+        print("TASK:")
+        print(input)
+        print("Planner Output:")
+        print("\n".join(planner_output_lst))
+        print("Planner Intermediate Steps:")
+        print("\n".join(planner_intermediate_steps_lst))
+        print("Controller Output:")
+        print("\n".join(controller_output_lst))
+        print("Controller Intermediate Steps:")
+        print("\n".join(controller_intermediate_steps_lst))
+        print("Verifier Output:")
+        print("\n".join(verifier_output_lst))
+        print("Verifier Intermediate Steps:")
+        print("\n".join(verifier_intermediate_steps_lst))
+
         exit()
+    else:
+        plan = replan
