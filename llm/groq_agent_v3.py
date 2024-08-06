05335e59f22299efb477d3824506a08c38986603
justas145 <justasand1@gmail.com>
2024-08-06 18:49:03 +0200

    test script

--- llm/groq_agent_v3.py
+++ llm/groq_agent_v3.py
@@ -4,6 +4,7 @@ from agent_tools import (
     initialize_collection,
     agent_tools_list,
     GetConflictInfo,
+    receive_bluesky_output
 )
 import os
 import sys
@@ -31,10 +32,47 @@ import threading
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
+# Connect to BlueSky simulator
+client = Client()
+client.connect("127.0.0.1", 11000, 11001)
+client.update()
+# Initialize the client in agent_tools
+initialize_client(client)
+
+
 # Setup basic logging
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
+# logging.basicConfig(
+#     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+# )
+
+
+# Define a custom logging handler
+# class InMemoryLogHandler(logging.Handler):
+#     def __init__(self):
+#         super().__init__()
+#         self.logs = []
+
+#     def emit(self, record):
+#         # Here you could process the log record, e.g., check if it's an error, extract information, etc.
+#         log_entry = self.format(record)
+#         self.logs.append(log_entry)  # Store log entries in a list
+#         print(log_entry)  # Optionally print the log entry
+
+
+# # Configure logging to use the custom handler
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
+# handler = InMemoryLogHandler()
+# formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
+
+
+def log_monitor():
+    """Function to simulate log generation, replace this with actual logging from your application"""
+    while True:
+        logging.info("Simulated log entry.")
+        time.sleep(5)  # Simulate logging every 5 seconds
 
 
 class CrashFileHandler(PatternMatchingEventHandler):
@@ -58,6 +96,42 @@ class CrashFileHandler(PatternMatchingEventHandler):
                     )  # Update the last position after reading
         except Exception as e:
             logging.error(f"Error reading file {event.src_path}: {e}")
+
+
+# Setup basic logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+class ListHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+        self.log_messages = []
+
+    def emit(self, record):
+        self.log_messages.append(self.format(record))
+
+
+def monitor_too_many_requests():
+    # Configure a logger for capturing specific log messages
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    list_handler = ListHandler()
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    list_handler.setFormatter(formatter)
+    logger.addHandler(list_handler)
+
+    while True:
+        # Simulating continuous log capturing
+        time.sleep(0.5)  # Check every 0.5 seconds for new logs
+        for log in list_handler.log_messages:
+            if "429 Too Many Requests" in log:
+                #print_output("Too many requests, pausing operations...")
+                client.send_event(b"STACK", "HOLD")
+                list_handler.log_messages.clear()  # Clear the log messages after handling
+                break
 
 
 def monitor_crashes(callback):
@@ -106,7 +180,7 @@ dotenv.load_dotenv("../.env")
 
 # llama3-70b-8192
 # llama3-groq-70b-8192-tool-use-preview
-temperature = 1.2
+temperature = 0.3
 model_name = "llama3-70b-8192"
 # llama-3.1-70b-versatile
 
@@ -125,19 +199,13 @@ openai_ef = chromadb.utils.embedding_functions.OpenAIEmbeddingFunction(
 
 
 # %%
-# Connect to BlueSky simulator
-client = Client()
-client.connect("127.0.0.1", 11000, 11001)
-client.update()
-# Initialize the client in agent_tools
-initialize_client(client)
 
 
 # %%
 # Vector DB
 collections = chroma_client.list_collections()
 collection_names = [collection.name for collection in collections]
-selected_collection = "skill_manuals"
+selected_collection = "experience_library_v1"
 
 collection = chroma_client.get_or_create_collection(
     name=selected_collection,
@@ -152,31 +220,20 @@ initialize_collection(collection)
 
 prompt = hub.pull("hwchase17/openai-tools-agent")
 
-with open("prompts/system.txt", "r") as f:
+with open("prompts/system_with_exp_lib.txt", "r") as f:
     prompt.messages[0].prompt.template = f.read()
 
 # with open("prompts/conflict.txt", "r") as f:
 #     prompt.messages[0].prompt.template += f.read()
-agent_tools_list = agent_tools_list[:-1]
+
+del agent_tools_list[1]
+del agent_tools_list[-1]
+
+agent_tools_list = agent_tools_list
 
 
-from langchain_together import ChatTogether
-
-# choose from our 50+ models here: https://docs.together.ai/docs/inference-models
-# chat = ChatTogether(
-#     # together_api_key="YOUR_API_KEY",
-#     model="meta-llama/Meta-Llama-3-70B-Instruct-Turbo",
-# )
-chat = ChatGroq(temperature=temperature, model_name=model_name, api_key=os.getenv("GROQ_API_KEY_2"))
-chat = ChatOpenAI(temperature=0.3, model='gpt-4o')
-# from langchain_fireworks import Fireworks, ChatFireworks
-# from dotenv import load_dotenv, find_dotenv
-# import os
-
-# llm = ChatFireworks(
-#     api_key=os.getenv("FIREWORKS_API_KEY"),
-#     model="accounts/fireworks/models/llama-v3-70b-instruct",
-# )
+chat = ChatGroq(temperature=temperature, model_name=model_name, api_key=os.getenv("GROQ_API_KEY_3"))
+chat = ChatOpenAI(temperature=0.3, model="gpt-4o")
 
 
 agent = agents.create_openai_tools_agent(chat, agent_tools_list, prompt)
@@ -185,12 +242,24 @@ agent_executor = agents.AgentExecutor(
     agent=agent, tools=agent_tools_list, verbose=True)
 
 # %%
-user_input = "You are an air traffic controller with tools. Solve aircraft conflict. Solve until there are no more conflicts"
+user_input = "You are an air traffic controller with tools. Solve aircraft conflict. Solve until there are no more conflicts. "
 
 scenario = "case3"
-client.send_event(b"STACK", f"IC TEST/Big/ac_3/dH/head-on_10.scn")
+# ac_3_no_dH_head-on_3
+# ac_3_dH_t-formation_8
+# ac_3_dH_head-on_9
+# ac_3_no_dH_converging_2
+# ac_3_no_dH_parallel_4
+# ac_4_dH_head - on_10
+# ac_4_dH_t-formation_7
+# ac_4_no_dH_converging_5
+# ac_4_no_dH_parallel_1
+# ac_4_no_dH_t-formation_1
+# ac_4_no_dH_t-formation_3
+client.send_event(b"STACK", f"IC TEST/Big/ac_4/no_dH/t-formation_3.scn")
 time.sleep(1.5)
-client.update()
+useless = client.update()
+useless =  receive_bluesky_output()
 
 def run_ai():
     with CaptureAndPrintConsoleOutput() as output:
@@ -203,7 +272,9 @@ def run_ai():
 if __name__ == "__main__":
     # Run the crash monitoring in a background thread
     threading.Thread(target=monitor_crashes, args=(print_output,), daemon=True).start()
-
+    threading.Thread(target=monitor_too_many_requests, daemon=True).start()
+    # client.send_event(b"STACK", "HOLD")
+    # time.sleep(10)
 
     # Run the AI operations in the main thread
     run_ai()
