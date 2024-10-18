63844ab9dadc0b19485fb7992b6ce3de35a45c31
justas145 <justasand1@gmail.com>
2024-10-18 22:34:09 +0200

    run_experiments script update

--- src/run_experiments.sh
+++ src/run_experiments.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-temperatures=(1.2)
+# Define arrays for agent types, skill library usage, and scenarios
+agent_types=(
+    single_agent 
+)
+use_skill_lib=(
+    no
+)
+scenarios=(
+    TEST/Big/ac_3/dH/head-on_10.scn
 
-for temp in "${temperatures[@]}"
+)
+
+# model_names=(
+#     "llama3-70b-8192"
+# )
+
+# Iterate through each scenario
+for i in "${!scenarios[@]}"
 do
     PYTHONDONTWRITEBYTECODE=1 python conflict_main.py \
-    --model_name mixtral-8x7b-32768,llama3-8b-8192,gpt-4o-2024-08-06,llama3-70b-8192,gemma2-9b-it \
-    --temperature $temp \
-    --scenario TEST/Small/ac_4 \
-    --output_csv results/test_small_all_models.csv
+    --agent_type "${agent_types[$i]}" \
+    --use_skill_lib "${use_skill_lib[$i]}" \
+    --scenario "${scenarios[$i]}" \
+    --output_csv results/good_configs_rerun_copy.csv
 done
